7f07baff526e165309fd7d29bd0fc50feb42f691
asserty308 <yannickassert@gmail.com>
2019-12-30 12:32:36 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:lyrics/features/search/ui/screens/search_song_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lyrics App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SearchSongScreen(),
+    );
+  }
+}
