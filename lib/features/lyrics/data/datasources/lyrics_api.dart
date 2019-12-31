f535082b14878d8f4ec1afd67f02d4d0007d7906
asserty308 <yannickassert@gmail.com>
2019-12-31 13:03:50 +0100

    refactoring

--- lib/features/lyrics/data/datasources/lyrics_api.dart
+++ lib/features/lyrics/data/datasources/lyrics_api.dart
@@ -1,16 +1,11 @@
 import 'package:flutter_core/web/base_api.dart';
 import 'package:lyrics/features/lyrics/data/models/lyrics_model.dart';
 
-class LyricsApi {
-  final _api = BaseApi('https://api.lyrics.ovh/v1/');
-
-  /// Wrapper to fetch JSON from the API
-  Future<dynamic> fetchJSON(String path) async {
-    return _api.fetchJSON(path);
-  }
+class LyricsApi extends BaseApi {
+  LyricsApi() : super('https://api.lyrics.ovh/v1/');
   
   Future<LyricsModel> fetchLyrics(String artist, String song) async {
-    final result = await _api.fetchJSON('$artist/$song');
+    final result = await fetchJSON('$artist/$song');
     return LyricsModel.fromJson(result);
   }
 }
--- lib/features/song_search/data/datasources/deezer_api.dart
+++ lib/features/song_search/data/datasources/deezer_api.dart
@@ -2,13 +2,8 @@ import 'package:flutter_core/web/base_api.dart';
 import 'package:lyrics/features/song_search/data/models/deezer_response_model.dart';
 import 'package:lyrics/features/song_search/data/models/song_data_model.dart';
 
-class DeezerApi {
-  final _api = BaseApi('api.deezer.com');
-
-  /// Wrapper to fetch JSON from the API
-  Future<dynamic> fetchJSON(String path, Map<String, String> query) async {
-    return _api.fetchJSON(path, query);
-  }
+class DeezerApi extends BaseApi {
+  DeezerApi() : super('api.deezer.com');
 
   Future<List<SongModel>> search(String query) async {
     final map = { 'q':query };
