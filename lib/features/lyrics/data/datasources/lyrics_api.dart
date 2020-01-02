94d0e17068dd0c0c6906f70720a5d66889a5f307
asserty308 <yannickassert@gmail.com>
2020-01-02 16:50:51 +0100

    translation for lyrics

--- lib/features/lyrics/data/datasources/lyrics_api.dart
+++ lib/features/lyrics/data/datasources/lyrics_api.dart
@@ -5,6 +5,10 @@ class LyricsApi extends BaseApi {
   LyricsApi() : super('https://api.lyrics.ovh/v1/');
   
   Future<LyricsModel> fetchLyrics(String artist, String song) async {
+    // remove '/' as it builds an invalid request
+    artist = artist.replaceAll('/', '');
+    song = song.replaceAll('/', '');
+
     final result = await fetchJSON('$artist/$song');
     return LyricsModel.fromJson(result);
   }
--- lib/features/lyrics/ui/screens/lyrics_screen.dart
+++ lib/features/lyrics/ui/screens/lyrics_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_core/modules/translation/data/translation_api.dart';
 import 'package:flutter_core/ui/widgets/center_progress_indicator.dart';
 import 'package:flutter_core/ui/widgets/center_text.dart';
 import 'package:lyrics/features/lyrics/data/datasources/lyrics_api.dart';
@@ -13,43 +14,76 @@ class LyricsScreen extends StatefulWidget {
 }
 
 class _LyricsScreenState extends State<LyricsScreen> {
-  final _api = LyricsApi();
-  var _lyrics = '';
+  final _lyricsApi = LyricsApi();
+  final _translationApi = TranslationApi();
+  var _originalLyrics = '';
+  var _translatedLyrics = '';
+  var _showTranslation = false;
+
+  String get _displayedLyrics {
+    return _showTranslation ? _translatedLyrics : _originalLyrics;
+  }
+
+  AppBar get _appBar {
+    return AppBar(
+      title: Text('${widget.song}'),
+      backgroundColor: Color.fromARGB(255, 1, 1, 1),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.favorite_border, color: Colors.white,),
+          onPressed: () {
+          },
+        ),
+        IconButton(
+          icon: Icon(Icons.translate, color: _showTranslation ? Colors.blue : Colors.white),
+          onPressed: () => _toggleTranslation(),
+        )
+      ],
+    );
+  }
 
   @override
   void initState() {
     super.initState();
-    fetchLyrics();
+    _fetchLyrics();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('${widget.artist} - ${widget.song}'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.favorite_border, color: Colors.white,),
-            onPressed: () {
-            },
-          )
-        ],
-      ),
-      body: _lyrics.isEmpty ? CenterProgressIndicator() : SingleChildScrollView(
+      appBar: _appBar,
+      body: _displayedLyrics.isEmpty ? CenterProgressIndicator() : SingleChildScrollView(
         padding: EdgeInsets.fromLTRB(8, 8, 8, 90), // content inset
         child: CenterText(
-          _lyrics,
+          _displayedLyrics,
           style: Theme.of(context).textTheme.title,
         ),
       ),
     );
   }
 
-  fetchLyrics() async {
-    final result = await _api.fetchLyrics(widget.artist, widget.song);
+  _fetchLyrics() async {
+    final result = await _lyricsApi.fetchLyrics(widget.artist, widget.song);
+    _originalLyrics = result.lyrics;
+    await _translateLyrics();
 
     setState(() {
-      _lyrics = result.lyrics ?? 'No lyrics found';
+    });
+  }
+
+  _translateLyrics() async {
+    var response = await _translationApi.translateText(_originalLyrics, Localizations.localeOf(context));
+
+    if (response == null || response.isEmpty) {
+      response = 'An error occured';
+    }
+
+    _translatedLyrics = response;
+  }
+
+  _toggleTranslation() {
+    setState(() {
+      _showTranslation = !_showTranslation;
     });
   }
 }
